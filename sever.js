bf35c3ff3cf10de80570f1e3d0286aabc0cc492a
Ha1ne99 <mylifedeadgame@gmail.com>
2026-03-06 23:06:16 +0200

    Update sever.js

--- sever.js
+++ sever.js
@@ -327,6 +327,8 @@ wss.on('connection', ws => {
 
 /* ================= START ================= */
 
-server.listen(3000, () => {
-  console.log('✅ http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
 });
